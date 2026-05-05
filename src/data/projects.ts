cce859af4000a536aead338034a3f1e37e4cbde2
FloRobart <FlorisRobart.pro@gmail.com>
2026-05-06 00:42:00 +0200

    feat: Ajouter un filtre pour les projets affichables et inclure la propriété 'displayable' dans les données des projets

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -18,6 +18,7 @@ export interface ProjectPreview {
   localLink: string;
   githubLink: string;
   projectLink?: string;
+  displayable: boolean;
 }
 
 export const projectsPreviewList: ProjectPreview[] = [
@@ -29,17 +30,19 @@ export const projectsPreviewList: ProjectPreview[] = [
     imageSrc: FloraccessImage,
     localLink: "/projets/floraccess",
     githubLink: "https://github.com/FloRobart/FlorAccess",
+    displayable: true,
   },
-  // {
-  //   id: "flower-garden",
-  //   title: "Flower Garden",
-  //   description:
-  //     "<b>Flower Garden</b> est un wrapper qui me permet de <b>lancer et de gérer mes applications</b> de manière simple et rapide. C'est également un site de <b>présentation de mes projets</b>. Grâce au <b>enregistrement DNS</b> de mon domaine, Flower garden app <b>liste automatiquement tous les projets</b> que j'ai développé et qui sont hébergés sur mon serveur, en utilisant les sous-domaines correspondants.",
-  //   imageSrc: FlowerGardenImage,
-  //   localLink: "/projets/flower-garden",
-  //   githubLink: "https://github.com/FloRobart/Flower-garden-app",
-  //   projectLink: "https://florobart.fr/",
-  // },
+  {
+    id: "flower-garden",
+    title: "Flower Garden",
+    description:
+      "<b>Flower Garden</b> est un wrapper qui me permet de <b>lancer et de gérer mes applications</b> de manière simple et rapide. C'est également un site de <b>présentation de mes projets</b>. Grâce au <b>enregistrement DNS</b> de mon domaine, Flower garden app <b>liste automatiquement tous les projets</b> que j'ai développé et qui sont hébergés sur mon serveur, en utilisant les sous-domaines correspondants.",
+    imageSrc: FlowerGardenImage,
+    localLink: "/projets/flower-garden",
+    githubLink: "https://github.com/FloRobart/Flower-garden-app",
+    projectLink: "https://florobart.fr/",
+    displayable: false,
+  },
   {
     id: "flollylist",
     title: "Flollylist",
@@ -49,7 +52,7 @@ export const projectsPreviewList: ProjectPreview[] = [
     localLink: "/projets/flollylist",
     githubLink: "https://github.com/FloRobart/flollylist_app",
     projectLink: "https://flollylist.florobart.fr/",
-
+    displayable: true,
   },
   {
     id: "econoris",
@@ -60,6 +63,7 @@ export const projectsPreviewList: ProjectPreview[] = [
     localLink: "/projets/econoris",
     githubLink: "https://github.com/FloRobart/econoris_app",
     projectLink: "https://econoris.florobart.fr/",
+    displayable: true,
   },
   {
     id: "lyssecure",
@@ -69,6 +73,7 @@ export const projectsPreviewList: ProjectPreview[] = [
     imageSrc: LysSecureImage,
     localLink: "/projets/lyssecure",
     githubLink: "https://github.com/FloRobart/Lys_secure",
+    displayable: true,
   },
   {
     id: "documentation",
@@ -79,36 +84,45 @@ export const projectsPreviewList: ProjectPreview[] = [
     localLink: "/projets/documentation",
     githubLink: "https://github.com/FloRobart/Documentations",
     projectLink: "https://documentation.florobart.fr/",
+    displayable: true,
   },
-  // {
-  //   id: "maven-lite",
-  //   title: "Maven Lite",
-  //   description:
-  //     "<b>Maven Lite</b> est un <b>logiciel en ligne de commande</b> de <b>construction de projets Java</b> qui permet de <b>gérer les dépendances</b> locales d'un projet, de le <b>compiler</b> et de l'<b>exécuter</b> très <b>facilement</b> et sans grande connaissance préalable, 5 à <b>10 minutes</b> suffisent <b>pour apprendre à l'utiliser</b>.<br><em>Ce projet est fonctionnel mais n'est pas terminé</em>",
-  //   imageSrc: MavenLiteImage,
-  //   localLink: "/projets/maven-lite",
-  //   githubLink: "https://github.com/FloRobart/Maven_lite",
-  // },
-  // {
-  //   id: "alloscp",
-  //   title: "All'OScp",
-  //   description:
-  //     "<b>AllOScp</b> (All OS cp) est un <b>client FTP et SFTP multi-plateforme</b> (Linux, Windows et MacOS) qui permet de <b>transférer des fichiers</b> entre un ordinateur et un serveur distant ou en local. Il permet également de <b>faciliter la mise en place de backup</b>, ainsi que la <b>comparaison de dossiers et de fichier</b> grâce à un système de comparaison au niveau de la ligne similaire à celui de Git.<br><em>Ce projet est fonctionnel mais n'est pas terminé</em>",
-  //   imageSrc: AlloscpImage,
-  //   localLink: "/projets/alloscp",
-  //   githubLink: "https://github.com/FloRobart/AllOScp",
-  // },
-  // {
-  //   id: "theme-vscode",
-  //   title: "Thème pour Visual Studio Code",
-  //   description:
-  //     "Ce projet est <b>mon thème personnalisé</b> pour Visual Studio Code, qui est un <b>thème sombre</b> avec des <b>couleurs vives</b> et <b>contrastées</b> pour une <b>meilleure lisibilité</b> du code. Il se base sur l'<b>extension 'GitHub.github-vscode-theme'</b>, par conséquent il ne peux pas être mis sur market place de Visual Studio Code. Il est fourni avec un <b>script d'installation bash</b> qui permet d'installer l'extension s'il n'est pas déjà installé puis installe ensuite le thème personnalisé nommé <b>'Github dark perso'</b>.",
-  //   imageSrc: ThemeVscodeImage,
-  //   localLink: "/projets/theme-vscode",
-  //   githubLink: "https://github.com/FloRobart/Themes_for_vsCode",
-  // },
+  {
+    id: "maven-lite",
+    title: "Maven Lite",
+    description:
+      "<b>Maven Lite</b> est un <b>logiciel en ligne de commande</b> de <b>construction de projets Java</b> qui permet de <b>gérer les dépendances</b> locales d'un projet, de le <b>compiler</b> et de l'<b>exécuter</b> très <b>facilement</b> et sans grande connaissance préalable, 5 à <b>10 minutes</b> suffisent <b>pour apprendre à l'utiliser</b>.<br><em>Ce projet est fonctionnel mais n'est pas terminé</em>",
+    imageSrc: MavenLiteImage,
+    localLink: "/projets/maven-lite",
+    githubLink: "https://github.com/FloRobart/Maven_lite",
+    displayable: false,
+  },
+  {
+    id: "alloscp",
+    title: "All'OScp",
+    description:
+      "<b>AllOScp</b> (All OS cp) est un <b>client FTP et SFTP multi-plateforme</b> (Linux, Windows et MacOS) qui permet de <b>transférer des fichiers</b> entre un ordinateur et un serveur distant ou en local. Il permet également de <b>faciliter la mise en place de backup</b>, ainsi que la <b>comparaison de dossiers et de fichier</b> grâce à un système de comparaison au niveau de la ligne similaire à celui de Git.<br><em>Ce projet est fonctionnel mais n'est pas terminé</em>",
+    imageSrc: AlloscpImage,
+    localLink: "/projets/alloscp",
+    githubLink: "https://github.com/FloRobart/AllOScp",
+    displayable: false,
+  },
+  {
+    id: "theme-vscode",
+    title: "Thème pour Visual Studio Code",
+    description:
+      "Ce projet est <b>mon thème personnalisé</b> pour Visual Studio Code, qui est un <b>thème sombre</b> avec des <b>couleurs vives</b> et <b>contrastées</b> pour une <b>meilleure lisibilité</b> du code. Il se base sur l'<b>extension 'GitHub.github-vscode-theme'</b>, par conséquent il ne peux pas être mis sur market place de Visual Studio Code. Il est fourni avec un <b>script d'installation bash</b> qui permet d'installer l'extension s'il n'est pas déjà installé puis installe ensuite le thème personnalisé nommé <b>'Github dark perso'</b>.",
+    imageSrc: ThemeVscodeImage,
+    localLink: "/projets/theme-vscode",
+    githubLink: "https://github.com/FloRobart/Themes_for_vsCode",
+    displayable: false,
+  },
 ];
 
 export function getProjectById(projectId: string): ProjectPreview | undefined {
-  return projectsPreviewList.find((project) => project.id === projectId);
+  try {
+    return projectsPreviewList.find((project) => project.id === projectId);
+  } catch (error) {
+    console.error(`Error occurred while fetching project with ID ${projectId}:`, error);
+    return undefined;
+  }
 }
