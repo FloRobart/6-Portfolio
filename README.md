06efa4fb756f0ea04582c2496190bbac77d394d6
FloRobart <FlorisRobart.pro@gmail.com>
2026-05-07 00:43:11 +0200

    feat: Ajouter des sections de compétences avec des compétences techniques et humaines, incluant des images et des descriptions

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # Portfolio ISCOD
+
+## Table des matières
+
+- [Portfolio ISCOD](#portfolio-iscod)
+  - [Table des matières](#table-des-matières)
+  - [Présentation](#présentation)
+  - [Construire le projet](#construire-le-projet)
+  - [Lancement du projet](#lancement-du-projet)
+    - [Mode développement](#mode-développement)
+    - [Mode production (Docker)](#mode-production-docker)
+  - [Mise à jour du projet](#mise-à-jour-du-projet)
+    - [Mode développement](#mode-développement-1)
+    - [Mode production](#mode-production)
+
+## Présentation
+
+Ce dépot est mon portfolio réalisé dans le cadre de ma formation de mastère en ingénierie logicielle à ISCOD.
+
+## Construire le projet
+
+Vous n'êtes pas obligé de construire le projet pour le lancer, que se sois en mode développement ou en mode production, mais si vous souhaitez le faire, voici les étapes à suivre :
+
+- installez les dépendances
+
+    ```bash
+    npm install
+    ```
+
+- construisez le projet
+
+    ```bash
+    npm run build
+    ```
+
+- Vous pouvez ensuite prendre vos fichiers statiques dans le dossier `dist` et les servir avec un serveur web comme nginx ou apache ou même les servir depuis des services de hosting comme github pages.
+
+## Lancement du projet
+
+### Mode développement
+
+- Installez les dépendances
+
+    ```bash
+    npm install
+    ```
+
+- Lancez le projet en mode développement
+
+    ```bash
+    npm run dev
+    ```
+
+- Ouvrez votre navigateur à l'adresse suivante : [http://localhost:4321](http://localhost:4321)
+
+### Mode production (Docker)
+
+- Utilisez un reverse proxy comme traefik ou nginx pour rediriger les requêtes vers le serveur nginx qui sert les fichiers statiques générés par le projet.
+- Lancé le projet en mode production
+
+    ```bash
+    ./run.sh
+    ```
+
+## Mise à jour du projet
+
+### Mode développement
+
+- La mise à jour se fait automatiquement grâce à la commande `npm run dev` qui utilise Vite pour recharger les fichiers à chaque changement.
+
+### Mode production
+
+- Lancez la commande `./run.sh` pour mettre à jour le projet en mode production. Cette commande va reconstruire le projet et redémarrer le serveur nginx pour servir les nouveaux fichiers statiques.
--- src/data/competences.ts
+++ src/data/competences.ts
@@ -2,24 +2,115 @@ import type { ImageMetadata } from "astro";
 
 import FloraccessImage from "@assets/projects/floraccess/floraccess_1_white.png";
 
-export interface ProjectPreview {
+import FlutterImage from "@assets/competences/flutter.png";
+import NodejsImage from "@assets/competences/nodejs.png";
+import DockerImage from "@assets/competences/docker.png";
+import LinuxImage from "@assets/competences/linux.jpeg";
+import DocumentationImage from "@assets/competences/documentation.jpg";
+
+export interface CompetencePreview {
   id: string;
   title: string;
   description: string;
   imageSrc: ImageMetadata;
   localLink: string;
-  githubLink: string;
-  projectLink?: string;
+  externalLink?: string;
+  type: "hard" | "soft";
+  displayable: boolean;
 }
 
-export const projectsPreviewList: ProjectPreview[] = [
+export const competencesPreviewList: CompetencePreview[] = [
+  /* Soft skills */
   {
     id: "organisation",
     title: "Organisation",
-    description:
-      "<b>FlorAccess</b> est avant tout une <b>API d'authentification</b> utilisée par mes autres projets. Elle permet un <b>accès sécurisé</b> et <b>sans mot de passe</b> à toutes mes applications, le tout avec un seul compte utilisateur. <b>FlorAccess</b> est également une <b>application d'administration</b> disponible seulement pour moi et qui me permet de <b>gérer les utilisateurs</b>.",
+    description: "",
     imageSrc: FloraccessImage,
     localLink: "/projets/floraccess",
-    githubLink: "https://github.com/FloRobart/FlorAccess",
+    type: "soft",
+    displayable: true,
+  },
+  {
+    id: "bienveillance",
+    title: "Bienveillance",
+    description: "",
+    imageSrc: FloraccessImage,
+    localLink: "/projets/floraccess",
+    type: "soft",
+    displayable: true,
+  },
+  {
+    id: "ingéniosité",
+    title: "Ingéniosité",
+    description: "",
+    imageSrc: FloraccessImage,
+    localLink: "/projets/floraccess",
+    type: "soft",
+    displayable: true,
+  },
+  {
+    id: "polivalence",
+    title: "Polivalence",
+    description: "",
+    imageSrc: FloraccessImage,
+    localLink: "/projets/floraccess",
+    type: "soft",
+    displayable: true,
+  },
+  {
+    id: "gestion-de-projet",
+    title: "Gestion de projet",
+    description: "",
+    imageSrc: FloraccessImage,
+    localLink: "/projets/floraccess",
+    type: "soft",
+    displayable: true,
+  },
+
+  /* Hard skills */
+  {
+    id: "flutter",
+    title: "Flutter",
+    description: "",
+    imageSrc: FlutterImage,
+    localLink: "/projets/floraccess",
+    type: "hard",
+    displayable: true,
+  },
+  {
+    id: "nodejs",
+    title: "NodeJS",
+    description: "",
+    imageSrc: NodejsImage,
+    localLink: "/projets/floraccess",
+    type: "hard",
+    displayable: true,
+  },
+  {
+    id: "docker",
+    title: "Docker",
+    description: "",
+    imageSrc: DockerImage,
+    localLink: "/projets/floraccess",
+    type: "hard",
+    displayable: true,
+  },
+  {
+    id: "linux",
+    title: "Linux",
+    description: "",
+    imageSrc: LinuxImage,
+    localLink: "/projets/floraccess",
+    type: "hard",
+    displayable: true,
+  },
+  {
+    id: "documentation",
+    title: "Documentation",
+    description: "",
+    imageSrc: DocumentationImage,
+    localLink: "/projets/floraccess",
+    type: "hard",
+    displayable: true,
   },
 ];
