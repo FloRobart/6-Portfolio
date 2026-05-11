881dcef4d89a18a78942554aefb3489fdcc256e1
FloRobart <FlorisRobart.pro@gmail.com>
2026-05-11 16:19:43 +0200

    feat: mettre à jour le contenu des sections et améliorer la navigation

---
+++ src/data/header_navigation.ts
@@ -0,0 +1,10 @@
+/**
+ * Ce fichier contient les données de navigation pour le header du portfolio.
+ * Chaque objet dans le tableau représente un lien de navigation avec une étiquette et une URL.
+ */
+export const navigations = [
+  { label: "Accueil", href: "/" },
+  { label: "Parcours", href: "/parcours" },
+  { label: "Réalisations", href: "/projets" },
+  { label: "Compétences", href: "/competences" },
+];
