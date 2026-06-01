d743c2afe80cd3900bdbf37e5091c1874dfde9db
FloRobart <florisrobart.pro@gmail.com>
2026-06-01 18:16:37 +0200

    feat: add new icons and update About, Footer, Social Link, and Presentation components
    - Added new Docker icons for red and white themes.
    - Updated the About component with improved text clarity and grammar.
    - Refactored the Footer component to use a centralized social links data file.
    - Enhanced the Social Link component to utilize the new social links data structure.
    - Improved the Presentation Content component with better text structure and grammar.
    - Created a new social_links.ts file to manage social media links and icons.
    - Updated the 404 page to include a button for navigation back to the homepage.
    - Minor text adjustments in the index.astro for better readability.

---
+++ src/data/social_links.ts
@@ -0,0 +1,48 @@
+import githubIcon from "@assets/icons_red/github.png";
+import dockerIcon from "@assets/icons_red/docker.png";
+import linkedinIcon from "@assets/icons_red/linkedin.png";
+import gmailIcon from "@assets/icons_red/gmail.png";
+import cvIcon from "@assets/icons_red/cv_icon.png";
+import lettreRecommandationIcon from "@assets/icons_red/lettre_recommandation.png";
+
+/**
+ * Ce fichier contient les liens vers les réseaux sociaux et autres ressources de contact, avec leurs icônes et descriptions.
+ */
+export const socialLinks = [
+  {
+    title: "Github",
+    href: "https://github.com/FloRobart",
+    icon: githubIcon,
+    alt: "Github",
+  },
+  {
+    title: "Docker Hub",
+    href: "https://hub.docker.com/u/florobart",
+    icon: dockerIcon,
+    alt: "Docker Hub",
+  },
+  {
+    title: "Linkedin",
+    href: "https://www.linkedin.com/in/floris-robart",
+    icon: linkedinIcon,
+    alt: "Linkedin",
+  },
+  {
+    title: "Envoyer un mail",
+    href: "mailto:florisrobart.pro@gmail.com",
+    icon: gmailIcon,
+    alt: "Gmail",
+  },
+  // {
+  //   title: "CV pdf",
+  //   href: "/pdf/CV_Floris_Robart.pdf",
+  //   icon: cvIcon,
+  //   alt: "CV",
+  // },
+  // {
+  //   title: "Lettre de recommandation",
+  //   href: "",
+  //   icon: lettreRecommandationIcon,
+  //   alt: "Lettre de recommandation",
+  // },
+];
